dc20ad50dc3fcffc2d40518f72cca7877092db3f
Eduardo <esep@uc.cl>
2025-07-16 18:21:41 -0400

    style(MainPage): home section

--- src/components/MainPage/HomeSection/HomeSection.jsx
+++ src/components/MainPage/HomeSection/HomeSection.jsx
@@ -4,6 +4,12 @@ export default function HomeSection() {
   return (
     <div className='home'>
 
+      <div className='titleBar'>
+        <h1>¡Hazte con la victoria usando el poder de tu ejército!</h1>
+      </div>
+
+      <button>Comenzar</button>
+      
     </div>
   )
 }
